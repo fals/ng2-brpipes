15f28673da083e54f78da1604c0d4570d09dfd23
Filipe Augusto Lima de Souza <filipe.augusto@squadra.com.br>
2016-12-26 19:34:16 -0200

    Cleaning up

--- src/cep.pipe.d.ts
+++
@@ -1,4 +0,0 @@
-import { PipeTransform } from '@angular/core';
-export declare class CepPipe implements PipeTransform {
-    transform(value: string): any;
-}
--- test/cep.pipe.spec.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var cnpj_pipe_1 = require('../src/cnpj.pipe');
-describe('CnpjPipe', function () {
-    var pipe = new cnpj_pipe_1.CnpjPipe();
-    it('transforms "43146822000198" to "43.146.822/0001-98"', function () {
-        expect(pipe.transform('43146822000198')).toBe('43.146.822/0001-98');
-    });
-});
-//# sourceMappingURL=cep.pipe.spec.js.map
